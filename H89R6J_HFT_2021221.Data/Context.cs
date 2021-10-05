4c0209d48aa7ab97f637db15ece0f6f14e820130
ThtBnc <tothbence@stud.uni-obuda.hu>
2021-10-05 19:24:45 +0200

    Everything for the first milestone, plus grabbed the connection string

---
+++ H89R6J_HFT_2021221.Data/Context.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H89R6J_HFT_2021221.Data
+{
+    class Context :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.
+                    UseLazyLoadingProxies().
+                    UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database.mdf; Integrated Security=True");
+            }
+        }
+    }
+}
