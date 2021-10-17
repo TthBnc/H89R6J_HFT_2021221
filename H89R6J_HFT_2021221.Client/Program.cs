6f1f0e8842dadea9ecd3edc05df4e6d34188a3c3
ThtBnc <tothbence@stud.uni-obuda.hu>
2021-10-17 17:51:13 +0200

    Context filled with data, relationships between Brand - Car and Car - Engine createad.

--- H89R6J_HFT_2021221.Client/Program.cs
+++ H89R6J_HFT_2021221.Client/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using H89R6J_HFT_2021221.Data;
+using System;
+using System.Linq;
 
 namespace H89R6J_HFT_2021221.Client
 {
@@ -6,7 +8,10 @@ namespace H89R6J_HFT_2021221.Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CarShopContext ctx = new CarShopContext();
+
+            var res1 = ctx.Cars.ToList();
+            ;
         }
     }
 }
