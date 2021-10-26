80d25e08a27cddcca72075d7e3dbf89775344fb3
tothbence@stud.uni-obuda.hu <tothbence@stud.uni-obuda.hu>
2021-10-26 11:36:34 +0200

    CarRepository and ICarRepository

---
+++ H89R6J_HFT_2021221.Repository/CarRepository.cs
@@ -0,0 +1,54 @@
+﻿using H89R6J_HFT_2021221.Data;
+using H89R6J_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H89R6J_HFT_2021221.Repository
+{
+    //kell a carshopcontext és igazándiból a benne lévő dolgokat
+    //car, brand és mittudomén kell
+    class CarRepository : ICarRepository
+    {
+        CarShopContext context;
+        public CarRepository(CarShopContext ctx)
+        {
+            context = ctx;
+        }
+        public void Create(Car car)
+        {
+            context.Cars.Add(car);
+            context.SaveChanges();
+        }
+
+        public Car ReadOne(int id)
+        {
+            return context
+                .Cars
+                .FirstOrDefault(c => c.Id == id);
+        }
+        public IQueryable<Car> ReadAll()
+        {
+            return context.Cars;
+        }
+
+        public void Update(Car car)
+        {
+            Car old = ReadOne(car.Id);
+
+            old.Model = car.Model;
+            old.BasePrice = car.BasePrice;
+            old.BrandId = car.BrandId;
+
+            context.SaveChanges();
+        }
+
+        public void Delete(int carId)
+        {
+            context.Cars.Remove(ReadOne(carId));
+            context.SaveChanges();
+        }
+    }
+}
---
+++ H89R6J_HFT_2021221.Repository/ICarRepositorycs.cs
@@ -0,0 +1,18 @@
+﻿using H89R6J_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H89R6J_HFT_2021221.Repository
+{
+    interface ICarRepository
+    {
+        void Create(Car car);
+        Car ReadOne(int id);
+        IQueryable<Car> ReadAll();
+        void Update(Car car);
+        void Delete(int carId);
+    }
+}
